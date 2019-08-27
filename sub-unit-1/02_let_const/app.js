ff0acbd070c22294c706d5e6b3df7e3ec9641338
Anthony Arias <aarias011@west-mec.org>
2019-08-26 17:43:01 -0700

    Messed with code:
    Inserted code to understand concept.

--- sub-unit-1/02_let_const/app.js
+++ sub-unit-1/02_let_const/app.js
@@ -1,19 +1,48 @@
 "use strict";
 
-var a = 'Test1';
-var b = 'Test2';
+// var a = 'Test1';
+// var b = 'Test2';
 
-console.log(`Logged: ${a} `);
+// console.log(`Logged: ${a} `);
 
-function testVar() {
-    let a = 30;
-    console.log(`Function Logged: ${a}`);
-    if (true) {
-        var a = 50;
-        console.log(`Block Logged: ${a}`);
+// function testVar() {
+//     let a = 30;
+//     console.log(`Function Logged: ${a}`);
+//     if (true) {
+//         var a = 50;
+//         console.log(`Block Logged: ${a}`);
+//     }
+//     console.log("here: "+a);
+// }
+// testVar();
+// console.log(`END Logged: ${a}`);
+
+let LOG = console.log;
+
+const pizza = [1,2,3];
+
+console.log(pizza);
+
+
+const pizza = [
+    {
+        name: "mango",
+        number: 3,
+        price: 5
+    },
+    {
+        name: "mushroom",
+        number: 3,
+        price: 5
+    },
+    {
+        name: "mango",
+        number: 3,
+        price: 5
     }
-    console.log("here: "+a);
-}
-testVar();
-console.log(`END Logged: ${a}`);
+];
 
+
+const [pep, mushroom] = pizza;
+
+LOG(mushroom);
